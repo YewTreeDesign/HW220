ed895c2936bd441e0242940d13b112a3c2b922ca
Charlene <nintendocandy@gmail.com>
2019-08-28 11:22:14 -0600

    homework

---
+++ HW1/HW1.js
@@ -0,0 +1,31 @@
+int x = 300;
+
+void setup()
+{
+  //background//
+  size(400,400);
+  background(255,153,102);
+  stroke(50);
+}
+void draw()
+{
+  //triangles//
+  triangle(100,200,300,200,200,400);
+  fill(255,229,204);
+  //Draw circle & place it: x,y,z//
+
+  //Cyan Circle//
+
+  circle(200,200,100);
+  noStroke();
+  fill(202,240,235);
+
+ //How do you even triangle...//
+
+ //Purple Circle//
+
+ circle(100,100,100);
+ noStroke();
+ fill(220,187,226);
+
+ //White Circle//
